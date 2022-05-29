c825651e8933044d9aa47279f1ca806d35c8a677
s4ndm4n82 <sadaruwan12@gmail.com>
2022-05-29 22:06:30 +0530

    Add project files.

---
+++ Report Cooker/Program.cs
@@ -0,0 +1,46 @@
+﻿using RptFunctionsSetClass;
+
+//Variable declaration.
+//For the MC.
+string WorkingDirectory = Directory.GetCurrentDirectory(); //Current working directory.
+string? ProgramfilesPath = Environment.GetEnvironmentVariable("ProgramFiles(x86)"); //Program Files path from Windows.
+string CapExecutablePath = @$"{ProgramfilesPath}\Navitro\CAP\"; //Concating the path with the program files path.
+string ExeName = "ManagementConsole.exe"; //CAP execuable name.
+string ReportCreateParam = "/CreateReport"; //Report parametere.
+
+//TODO: For database detail get from secrets.
+
+//Used field.
+string UsedFieldName = "Voucher Type";
+
+//Main report output folder path.
+string MainReportOutputPath = Path.Combine(WorkingDirectory,"Reports");
+
+//Report output type array.
+string[] ReportOutTypes = new string[] { "PDF", "EXCEL", "WORD" };
+
+//Report name array.
+string[] ReportNames = new string[] {"Items Per Job",
+                                    "Items Per Job Detailed",
+                                    "Imported Exported",
+                                    "GroupBy Fields",
+                                    "Group By Fields Job Columns",
+                                    "Group By Fields Job Columns",
+                                    "Audit Log Field Changes",
+                                    "Audit Log Count",
+                                    "Time Spent"};
+
+//Report output folder nmaes.
+string[] ReportOutputFolderNames = new string[] {"ItemsPerJob",
+                                    "ItemsPerJobDetailed",
+                                    "ImportedExported",
+                                    "GroupByFields",
+                                    "GroupByFieldsJobColumns",
+                                    "GroupByFieldsJobColumns",
+                                    "AuditLogFieldChanges",
+                                    "AuditLogCount",
+                                    "TimeSpent"};
+
+RptFunctionsSet.MainBannaer();
+
+RptFunctionsSet.FolderCheck(MainReportOutputPath, ReportOutputFolderNames);
---
+++ Report Cooker/RptFunctionsSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RptFunctionsSetClass
+{
+    internal class RptFunctionsSet
+    {
+        //The banner.
+        public static void MainBannaer()
+        {
+            var NewLine = Environment.NewLine;
+            Console.WriteLine(
+                "                                  ******************************************************{0}" +
+                "                                  *                                                    *{0}" +
+                "                                  *               Manual Reports Generator             *{0}" +
+                "                                  *                                                    *{0}" +
+                "                                  *      Created by: Sandaruwan Samaraweera            *{0}" +
+                "                                  *      Version: 1.0.1                                *{0}" +
+                "                                  *                                                    *{0}" +
+                "                                  ******************************************************{0}" +
+                "------------------------------------------------------------------------------------------------------------------------ {0}", NewLine);
+
+        }
+
+        //Checks and creates the needed folders if missing.
+        public static void FolderCheck(string _MainOutputPath, string[] _OutputFolderNames)
+        {
+            if (!Directory.Exists(_MainOutputPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(_MainOutputPath);
+                    Console.WriteLine("Main Report Folder Created at {0}", _MainOutputPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception thrown: {0}", ex.Message);
+                }
+            }
+            
+            foreach (string _OuptuFolderName in _OutputFolderNames)
+            {
+                string _OutputFolderPath = Path.Combine(_MainOutputPath, _OuptuFolderName);
+
+                if (!Directory.Exists(_OutputFolderPath))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(_OutputFolderPath);
+                        Console.WriteLine("Folder {0} created at {1}",_OuptuFolderName, _OutputFolderPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception throen: {0}", ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
